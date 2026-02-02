64a25977cc5cb3574b53f290490f166d3ac1ff65
SM mest3r <78697727+SMmest3r@users.noreply.github.com>
2026-02-02 17:09:43 +0100

    Refactored config; added on fix screen-flash and paint options

--- config.lua
+++ config.lua
@@ -1,30 +1,49 @@
 Config = {}
 
+Config.Blip = {
+	sprite = 361,
+	scale = 0.8,
+	colour = 1,
+	shortRange = true,
+	name = "Gas Station",
+}
+
 Config.Stations = {
-    vector3(263.894, 2606.463, 44.983), --Gas startion 1
-	vector3(1039.958, 2671.134, 39.550), --Gas startion 2
-	vector3(1207.260, 2660.175, 37.899), --Gas startion 3
-	vector3(2539.685, 2594.192, 37.944), --Gas startion 4
-	vector3(2679.858, 3263.946, 55.240), --Gas startion 5
-	vector3(2005.055, 3773.887, 32.403), --Gas startion 6
-	vector3(1687.156, 4929.392, 42.078), --Gas startion 7
-	vector3(1701.314, 6416.028, 32.763), --Gas startion 8
-	vector3(179.857, 6602.839, 31.868), --Gas startion 9
-	vector3(-94.4619, 6419.594, 31.489), --Gas startion 10
-	vector3(-2554.996, 2334.40, 33.078), --Gas startion 11
-	vector3(-1800.375, 803.661, 138.651), --Gas startion 12
-	vector3(-1437.622, -276.747, 46.207), --Gas startion 13
-	vector3(-2096.243, -320.286, 13.168), --Gas startion 14
-	vector3(-724.619, -935.1631, 19.213), --Gas startion 15
-	vector3(-526.019, -1211.003, 18.184), --Gas startion 16
-	vector3(-70.2148, -1761.792, 29.534), --Gas startion 17
-	vector3(265.648, -1261.309, 29.292), --Gas startion 18
-	vector3(819.653, -1028.846, 26.403), --Gas startion 19
-	vector3(1208.951, -1402.567,35.224), --Gas startion 20
-	vector3(1181.381, -330.847, 69.316), --Gas startion 21
-	vector3(620.843, 269.100, 103.089), --Gas startion 22
-	vector3(2581.321, 362.039, 108.468), --Gas startion 23
-	vector3(176.631, -1562.025, 29.263), --Gas startion 24
-	vector3(-319.292, -1471.715, 30.549), --Gas startion 25
-	vector3(1784.324, 3330.55, 41.253) --Gas startion 26
+	[1] = {coords = vector3(263.894, 2606.463, 44.983), radius = 20.0, blip = true},
+	[2] = {coords = vector3(1039.958, 2671.134, 39.550), radius = 20.0, blip = true},
+	[3] = {coords = vector3(1207.260, 2660.175, 37.899), radius = 20.0, blip = true},
+	[4] = {coords = vector3(2539.685, 2594.192, 37.944), radius = 20.0, blip = true},
+	[5] = {coords = vector3(2679.858, 3263.946, 55.240), radius = 20.0, blip = true},
+	[6] = {coords = vector3(2005.055, 3773.887, 32.403), radius = 20.0, blip = true},
+	[7] = {coords = vector3(1687.156, 4929.392, 42.078), radius = 20.0, blip = true},
+	[8] = {coords = vector3(1701.314, 6416.028, 32.763), radius = 20.0, blip = true},
+	[9] = {coords = vector3(179.857, 6602.839, 31.868), radius = 20.0, blip = true},
+	[10] = {coords = vector3(-94.4619, 6419.594, 31.489), radius = 20.0, blip = true},
+	[11] = {coords = vector3(-2554.996, 2334.40, 33.078), radius = 20.0, blip = true},
+	[12] = {coords = vector3(-1800.375, 803.661, 138.651), radius = 20.0, blip = true},
+	[13] = {coords = vector3(-1437.622, -276.747, 46.207), radius = 20.0, blip = true},
+	[14] = {coords = vector3(-2096.243, -320.286, 13.168), radius = 20.0, blip = true},
+	[15] = {coords = vector3(-724.619, -935.1631, 19.213), radius = 20.0, blip = true},
+	[16] = {coords = vector3(-526.019, -1211.003, 18.184), radius = 20.0, blip = true},
+	[17] = {coords = vector3(-70.2148, -1761.792, 29.534), radius = 20.0, blip = true},
+	[18] = {coords = vector3(265.648, -1261.309, 29.292), radius = 20.0, blip = true},
+	[19] = {coords = vector3(819.653, -1028.846, 26.403), radius = 20.0, blip = true},
+	[20] = {coords = vector3(1208.951, -1402.567,35.224), radius = 20.0, blip = true},
+	[21] = {coords = vector3(1181.381, -330.847, 69.316), radius = 20.0, blip = true},
+	[22] = {coords = vector3(620.843, 269.100, 103.089), radius = 20.0, blip = true},
+	[23] = {coords = vector3(2581.321, 362.039, 108.468), radius = 20.0, blip = true},
+	[24] = {coords = vector3(176.631, -1562.025, 29.263), radius = 20.0, blip = true},
+	[25] = {coords = vector3(-319.292, -1471.715, 30.549), radius = 20.0, blip = true},
+	[26] = {coords = vector3(1784.324, 3330.55, 41.253), radius = 20.0, blip = true},
+}
+
+Config.OnFix = {
+	sound = true, -- Play sound on vehicle fix
+	flash = {
+		enabled = true, -- Enable flash effect on vehicle fix
+		fadeIn = 0, -- Fade-in time for the flash effect in milliseconds
+		duration = 300, -- Duration of the flash effect in milliseconds
+		fadeOut = 0 -- Fade-out time for the flash effect in milliseconds
+	},
+	paint = true -- Sets the vehicle's color to a new random color on vehicle fix
 }
--- main.lua
+++ main.lua
@@ -1,13 +1,15 @@
 Citizen.CreateThread(function()
     for k,v in pairs(Config.Stations) do
-        local blip = AddBlipForCoord(v)
-        SetBlipSprite(blip, 361)
-        SetBlipScale(blip, 0.8)
-        SetBlipColour(blip, 1)
-        SetBlipAsShortRange(blip, true)
-        BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString("Gas Station")
-        EndTextCommandSetBlipName(blip)
+        if v.blip then
+            local blip = AddBlipForCoord(v.coords)
+            SetBlipSprite(blip, Config.Blip.sprite)
+            SetBlipScale(blip, Config.Blip.scale)
+            SetBlipColour(blip, Config.Blip.colour)
+            SetBlipAsShortRange(blip, Config.Blip.shortRange)
+            BeginTextCommandSetBlipName("STRING")
+            AddTextComponentString(Config.Blip.name)
+            EndTextCommandSetBlipName(blip)
+        end
     end
     local wait = 2000
     while true do
@@ -19,18 +21,30 @@ Citizen.CreateThread(function()
         end
         if wait == 500 then
             for k,v in pairs(Config.Stations) do
-                if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v, true) < 20 then
+                if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v.coords, true) < v.radius then
                     if IsPedInAnyVehicle(PlayerPedId()) then
                         local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
                         if GetVehicleEngineHealth(vehicle) < 1000.0 and GetVehicleEngineHealth(vehicle) > 0 then
-                            SendNUIMessage({
-                                type = "play",
-                            })
+                            if Config.OnFix.sound then
+                                SendNUIMessage({
+                                    type = "play",
+                                })
+                            end
+                            if Config.OnFix.flash.enabled then
+                                SetFlash(0, 0, Config.OnFix.flash.fadeIn, Config.OnFix.flash.duration, Config.OnFix.flash.fadeOut)
+                            end
                             SetVehicleEngineHealth(vehicle, 1000.0)
                             SetVehicleEngineOn(vehicle, true, true)
                             SetVehicleFixed(vehicle)
                             SetVehicleDeformationFixed(vehicle)
                             SetVehicleUndriveable(vehicle, false)
+                            if Config.OnFix.paint then
+                                local r = math.random(0, 255)
+                                local g = math.random(0, 255)
+                                local b = math.random(0, 255)
+                                SetVehicleCustomPrimaryColour(vehicle, r, g, b)
+                                SetVehicleCustomSecondaryColour(vehicle, r, g, b)
+                            end
                         end
                     end
                 end
