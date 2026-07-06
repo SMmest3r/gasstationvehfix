da792c7aefefd1944710fdd955f6c490d87279eb
SM mest3r <s.mate3d@gmail.com>
2026-07-06 12:53:21 +0200

    Added missing "," in Config

--- config.lua
+++ config.lua
@@ -50,7 +50,7 @@ Config.OnFix = {
 
 Config.Wait = {
 	inVehicle = 500, -- Wait time between checks when player is in a vehicle (in milliseconds) !Reducing this may impact performance!
-	outVehicle = 2000 -- Wait time between checks when player is not in a vehicle (in milliseconds) !Reducing this may impact performance!
+	outVehicle = 2000, -- Wait time between checks when player is not in a vehicle (in milliseconds) !Reducing this may impact performance!
 	coolwown = 45000 -- Wait time after a vehicle has been fixed before it can be fixed again (in milliseconds)
 }
 
